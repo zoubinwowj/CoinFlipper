c0d8dc4dcb079bb9d5fd581e1564a5945ee9ea9f
Devansh Chouhan <devanshchouhan7112003@gmail.com>
2024-09-01 19:46:12 +0530

    Update TopLeftBg.jsx

--- src/components/TopLeftBg.jsx
+++ src/components/TopLeftBg.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export default function TopLeftBg({className}) {
     return (
         <img
-            src="../src/assets/images/topleft.svg"
+            src="../../topleft.svg"
             alt="Top Left Decoration"
             className={className}
         />
