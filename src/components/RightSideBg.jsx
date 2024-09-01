c5df18620122251b6276afb3cc32dba272c944ac
Devansh Chouhan <devanshchouhan7112003@gmail.com>
2024-09-01 17:45:57 +0530

    Update RightSideBg.jsx

--- src/components/RightSideBg.jsx
+++ src/components/RightSideBg.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export default function RightSideBg({className}) {
   return (
     <img
-            src="/src/assets/images/rightbg.svg"
+            src="../src/assets/images/rightbg.svg"
             alt="Right Side Decoration"
             className={className}
         />
