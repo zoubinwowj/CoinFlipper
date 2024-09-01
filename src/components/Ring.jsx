a922c44a23e5213fb0c7c31be265f4469351dbbd
Devansh Chouhan <devanshchouhan7112003@gmail.com>
2024-09-01 11:09:04 +0530

    Update Ring.jsx

--- src/components/Ring.jsx
+++ src/components/Ring.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export default function Ring({ className }) {
   return (
     <img
-      src="../public/images/ringbg.webp"
+      src="../../public/images/ringbg.webp"
       alt="Ring"
       className={className}
     />
