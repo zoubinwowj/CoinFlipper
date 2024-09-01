00062a99823ead6c6cfffccc729c57d213fe8b9c
Devansh Chouhan <devanshchouhan7112003@gmail.com>
2024-09-01 19:47:27 +0530

    Update EtheruemLogo.jsx

--- src/components/EtheruemLogo.jsx
+++ src/components/EtheruemLogo.jsx
@@ -1,7 +1,7 @@
 export default function EtheruemLogo() {
   return (
     <div className="absolute inset-y-0 right-1 top-1 flex items-center h-8 w-8">
-      <img src="../src/assets/images/inputethereum.png" alt="" />
+      <img src="../../inputethereum.png" alt="" />
     </div>
 
   )
